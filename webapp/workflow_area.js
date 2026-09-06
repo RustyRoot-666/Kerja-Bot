509355a43d921a8f2fd20b70b980fdf2917f8dc1
RustyRoot-666 <tegethespecialist@gmail.com>
2026-09-06 16:39:09 +0700

    Bust Orderanku area filter after Smart Order UI integration

--- webapp/workflow_area.js
+++ webapp/workflow_area.js
@@ -85,4 +85,4 @@ renderWorkflowForm = function renderWorkflowFormWithAreaContext(action, order) {
 };
 
 // Load the broad operational Orderanku area filter after all app functions are defined.
-(function(){ const s=document.createElement('script'); s.src='/website/order_area_filter.js?v=20260906-broad5'; s.async=false; document.head.appendChild(s); })();
+(function(){ const s=document.createElement('script'); s.src='/website/order_area_filter.js?v=20260906-broad6'; s.async=false; document.head.appendChild(s); })();
