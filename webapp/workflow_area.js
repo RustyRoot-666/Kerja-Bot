5211af67b979e010dcd075d3e25a066fd43c0994
RustyRoot-666 <tegethespecialist@gmail.com>
2026-09-06 16:32:14 +0700

    Load broad Orderanku area filter

--- webapp/workflow_area.js
+++ webapp/workflow_area.js
@@ -84,5 +84,5 @@ renderWorkflowForm = function renderWorkflowFormWithAreaContext(action, order) {
   if (ticketBlock?.parentNode === article) ticketBlock.insertAdjacentElement('afterend', context); else article.insertBefore(context, article.querySelector('form'));
 };
 
-// Load the address-category override after all app functions are defined.
-(function(){ const s=document.createElement('script'); s.src='/website/order_area_filter.js?v=20260906-address4'; s.async=false; document.head.appendChild(s); })();
+// Load the broad operational Orderanku area filter after all app functions are defined.
+(function(){ const s=document.createElement('script'); s.src='/website/order_area_filter.js?v=20260906-broad5'; s.async=false; document.head.appendChild(s); })();
